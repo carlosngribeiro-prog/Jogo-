b38bd463bb5db4866afb03553a7c7a004afad426
carlosngribeiro-prog <carlos.ngribeiro@aluno.edu.es.gov.br>
2026-09-25 13:36:32 -0300

    gk

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
 const player = document.getElementById("player");
 const obstacle = document.getElementById("obstacle");
 
+
 const scoreElement = document.getElementById("score");
 const bestElement = document.getElementById("best");
+
 
 const gameOverScreen = document.getElementById("gameOver");
 const finalScore = document.getElementById("finalScore");
 
+
 let gameRunning = false;
 let gamePaused = false;
+
 
 let score = 0;
 let bestScore = 0;
 
+
 let obstaclePosition = 100;
 let speed = 2;
+
 
 let lastTime = 0;
 let animationId;
 
 
+
+
 // PULAR
 function jump() {
 
-    if (!gameRunning || gamePaused) {
-        return;
-    }
 
-    if (!player.classList.contains("jump")) {
+   if (!gameRunning || gamePaused) {
+       return;
+   }
 
-        player.classList.add("jump");
 
-        set
+   if (!player.classList.contains("jump")) {
+
+
+       player.classList.add("jump");
+
+
+       set
+
+
+
